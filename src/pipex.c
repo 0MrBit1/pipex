b3153524fb89ee114047e07e9be0d11251945b50
abdo <acharik@student.1337.ma>
2024-05-24 22:37:38 +0100

    pipex debug

--- src/pipex.c
+++ src/pipex.c
@@ -1,77 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acharik <acharik@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/24 22:34:40 by acharik           #+#    #+#             */
+/*   Updated: 2024/05/24 22:37:12 by acharik          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/pipex.h"
 
-void child_process1(char **argv , char **envp  , int *pipefd )
+void	child_process1(char **argv, char **envp, int *pipefd)
 {
-    char    *cmd;
-    char    **args;
-    int     fd; 
+	char	*cmd;
+	char	**args;
+	int		fd;
 
-    close(pipefd[0]);
-
-    args = ft_split(argv[2] , ' ');
-    fd = open(argv[1] , O_RDONLY); 
-    if (fd < 0)
-    { 
-        perror("error: \n");
-        exit(1);
-    }
-    dup_fds(fd ,  pipefd[1] , 0 , 1);
-    cmd =  return_command(args , envp);
-    execve(cmd , args , envp);
-    perror ("error exece cmd1 \n");
-    exit(1);
-}
-void child_process2(char **argv , char **envp  , int *pipefd )
-{
-    int     i;
-    char    *cmd;
-    char    **args;
-    int     fd;
-
-    close(pipefd[1]);
-    i = 0;
-    args = ft_split(argv[3] , ' ');
-    fd = open(argv[4] , O_WRONLY | O_CREAT | O_TRUNC, 0666) ;
-    if (fd < 0)
-        perror("error : \n");
-    cmd = return_command(args , envp);
-    dup_fds(fd ,  pipefd[0] , 1 , 0);
-    execve(cmd, args , envp);
-    perror ("error exece cmd2 \n");
-    exit (1);
-}
-void parent_process(int *pipefd , char **argv , char **envp )
-{
-    int i;
-    pid_t   pid;
-
-    i = 0;
-    while ( i < 2)
-    {
-        pid = fork() ;
-        if (pid < 0)
-        {
-            perror("Failed to fork child process\n");
-            exit(1);
-        }
-        if (pid == 0)
-        {
-            if (i == 0)
-                child_process1(argv , envp ,pipefd);
-            else if (i == 1)
-                child_process2(argv , envp  ,pipefd);
-        }
-        i++ ; 
-    }
-    close(pipefd[0]);
-    close(pipefd[1]); 
-    while(wait(NULL) != -1);
+	close(pipefd[0]);
+	args = ft_split(argv[2], ' ');
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+	{
+		perror("error: \n");
+		exit(1);
+	}
+	dup_fds(fd, pipefd[1], 0, 1);
+	cmd = return_command(args, envp);
+	execve(cmd, args, envp);
+	perror("error exece cmd1 \n");
+	free(cmd);
+	exit(1);
 }
 
-int main(int argc , char **argv , char **envp )
+void	child_process2(char **argv, char **envp, int *pipefd)
 {
-    int     pipefd[2];
-   
-    error_handler(argc , pipefd) ;
-    parent_process(pipefd , argv , envp );
+	int		i;
+	char	*cmd;
+	char	**args;
+	int		fd;
+
+	close(pipefd[1]);
+	i = 0;
+	args = ft_split(argv[3], ' ');
+	fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	if (fd < 0)
+		perror("error : \n");
+	cmd = return_command(args, envp);
+	dup_fds(fd, pipefd[0], 1, 0);
+	execve(cmd, args, envp);
+	free(cmd);
+	perror("error exece cmd2 \n");
+	exit(1);
+}
+
+void	parent_process(int *pipefd, char **argv, char **envp)
+{
+	int		i;
+	pid_t	pid;
+
+	i = 0;
+	while (i < 2)
+	{
+		pid = fork();
+		if (pid < 0)
+		{
+			perror("Failed to fork child process\n");
+			exit(1);
+		}
+		if (pid == 0)
+		{
+			if (i == 0)
+				child_process1(argv, envp, pipefd);
+			else if (i == 1)
+				child_process2(argv, envp, pipefd);
+		}
+		i++;
+	}
+	close(pipefd[0]);
+	close(pipefd[1]);
+	while (wait(NULL) != -1)
+		;
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	int	pipefd[2];
+
+	error_handler(argc, pipefd);
+	parent_process(pipefd, argv, envp);
 }
--- src/pipex_utils.c
+++ src/pipex_utils.c
@@ -1,76 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acharik <acharik@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/24 22:34:47 by acharik           #+#    #+#             */
+/*   Updated: 2024/05/24 22:37:04 by acharik          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/pipex.h"
 
-char *cmd_helper(char *cmd , char *PATH_VARIABLE , int start , int i )
+char	*cmd_helper(char *cmd, char *PATH_VARIABLE, int start, int i)
 {
-    char    *cmd_path;
-    char    *potencial_path;
+	char	*cmd_path;
+	char	*potencial_path;
 
-    while(PATH_VARIABLE[i])
-    {   
-        if (PATH_VARIABLE[i] == ':')
-        {
-            potencial_path = ft_substr(PATH_VARIABLE , start , i - start ) ;
-            cmd_path = ft_strjoin(potencial_path , cmd);
-            free(potencial_path);
-            if (!access(cmd_path , X_OK))
-            {
-                free(cmd);
-                return cmd_path;
-            }
-            else 
-                free(cmd_path);
-            start = ++i; 
-        }
-        i++;
-    }
-    return NULL ;
+	while (PATH_VARIABLE[i])
+	{
+		if (PATH_VARIABLE[i] == ':')
+		{
+			potencial_path = ft_substr(PATH_VARIABLE, start, i - start);
+			cmd_path = ft_strjoin(potencial_path, cmd);
+			free(potencial_path);
+			if (!access(cmd_path, X_OK))
+			{
+				free(cmd);
+				return (cmd_path);
+			}
+			else
+				free(cmd_path);
+			start = ++i;
+		}
+		i++;
+	}
+	return (NULL);
 }
-char *get_command_path(char *cmd , char *PATH_VARIABLE)
-{
-    char    *cmd_path;
 
-    if (!access(cmd , X_OK))
-        return cmd;
-    cmd = ft_strjoin("/" , cmd); 
-    cmd_path = cmd_helper(cmd , PATH_VARIABLE , 0 , 0 ); 
-    return cmd_path;
-}
-void dup_fds(int fd ,  int pipefd , int fd_dup , int pipe_dup )
+char	*get_command_path(char *cmd, char *PATH_VARIABLE)
 {
-    if (dup2(fd , fd_dup) < 0 )
-    {
-        perror("error: \n");
-        exit(1);
-    } 
-    if (dup2(pipefd , pipe_dup) < 0 )
-    {
-        perror("error: \n");
-        exit(1);
-    }
-}
-char *return_command(char **args , char **envp)
-{
-    int i ;
+	char	*cmd_path;
 
-    i = 0;
-    while(envp[i])
-    {
-        if (!  ft_strncmp( envp[i], "PATH=" , 5 ))
-            return get_command_path(args[0] , envp[i] + 5);
-        i++;
-    }
-    return NULL;
+	cmd = ft_strjoin("/", cmd);
+	cmd_path = cmd_helper(cmd, PATH_VARIABLE, 0, 0);
+	return (cmd_path);
 }
-void error_handler(int argc , int *pipefd)
+
+void	dup_fds(int fd, int pipefd, int fd_dup, int pipe_dup)
 {
-    if (argc != 5)
-    {
-        perror("Please follow this format : in_file cmd1 cmd2 out_file\n");
-        exit(1);
-    }
-    if ( pipe(pipefd) < 0 )
-    {
-        perror("There was an error creating the pipe\n"); 
-        exit(1);
-    }
+	if (dup2(fd, fd_dup) < 0)
+	{
+		perror("error: \n");
+		exit(1);
+	}
+	if (dup2(pipefd, pipe_dup) < 0)
+	{
+		perror("error: \n");
+		exit(1);
+	}
+}
+
+char	*return_command(char **args, char **envp)
+{
+	int	i;
+
+	i = 0;
+	if (!access(args[0], X_OK))
+		return (args[0]);
+	if (args[0][0] == '/' || args[0][0] == '.')
+		return (NULL);
+	while (envp[i])
+	{
+		if (!ft_strncmp(envp[i], "PATH=", 5))
+			return (get_command_path(args[0], envp[i] + 5));
+		i++;
+	}
+	return (NULL);
+}
+
+void	error_handler(int argc, int *pipefd)
+{
+	if (argc != 5)
+	{
+		perror("Please follow this format : in_file cmd1 cmd2 out_file\n");
+		exit(1);
+	}
+	if (pipe(pipefd) < 0)
+	{
+		perror("There was an error creating the pipe\n");
+		exit(1);
+	}
 }
